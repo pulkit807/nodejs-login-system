bfa995a65b793d8c2591529ae098114b48ac60ea
Pulkit0807 <pulkit4143kukreja@gmail.com>
2021-03-15 17:54:18 +0530

    login system

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const smsSecretKey = process.env.SMS_SECRET_KEY
 
 const app = express();
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+//app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(cookieParser())
 
 
